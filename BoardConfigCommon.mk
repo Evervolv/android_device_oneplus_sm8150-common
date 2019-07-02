dc88511f11773bfe89b5a9fc62727c193a9dbe7c
Lijuan Gao <lijuang@codeaurora.org>
2019-07-02 15:57:32 +0800

    msmnile: Set Header version for bootimage
    Set Header version to 1 for bootimage.
    
    Change-Id: Idd2c78689f292dbcdfa7f1eb0b91a8d91aac12c6
    Signed-off-by: Lijuan Gao <lijuang@codeaurora.org>
    
    msmnile: Add support for header version 2 of boot image
    
    Adding support to make a bootimage with header version as 2.
    This bootimage no longer has the dtbs appended to the kernel.
    All the dtbs are made into a new image called dtb.img and
    this image is present in the bootimage.
    
    Change-Id: I47867fc284a45bcf2e54af5f6cd40d894418c002
    Signed-off-by: Lijuan Gao <lijuang@codeaurora.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,7 +39,7 @@ TARGET_USES_UEFI := true
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048  loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += androidboot.vbmeta.avb_version=1.0
-BOARD_KERNEL_IMAGE_NAME := Image-dtb
+BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
@@ -81,6 +81,15 @@ AUDIO_FEATURE_ENABLED_INSTANCE_ID := true
 AUDIO_FEATURE_ENABLED_PROXY_DEVICE := true
 USE_CUSTOM_AUDIO_POLICY := 1
 USE_XML_AUDIO_POLICY_CONF := 1
+
+# Bootimage
+ifneq ($(filter Image.gz-dtb Image.lz4-dtb Image-dtb,$(BOARD_KERNEL_IMAGE_NAME)),)
+BOARD_BOOTIMG_HEADER_VERSION := 1
+else
+BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+endif
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 
 # Camera
 TARGET_USES_QTI_CAMERA_DEVICE := true
