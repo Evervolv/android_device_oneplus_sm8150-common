f3fe9fcb9e652de8fafc1252817965ee8d659d1e
Ashwin R C <ashwin2001achu@gmail.com>
2020-01-05 06:33:31 +0000

    sm8150-common: build vendor.display.config@1.10
    -Sync with manifest
    
    Change-Id: Ia0d6c27252fb9b6bfa62c02ceab43fcafef592ea

--- common.mk
+++ common.mk
@@ -239,7 +239,7 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     libvulkan \
     memtrack.msmnile \
-    vendor.display.config@1.7 \
+    vendor.display.config@1.10 \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.allocator@1.0-service \
     vendor.qti.hardware.display.allocator@1.0.vendor \
