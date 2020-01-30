0c9cae21143775f5d2d5a5eca9feebc36650f5e2
Nick Reuter <nreuter85@gmail.com>
2020-01-29 23:19:42 -0600

    sm8150-common: update security patch to 10.3.0
    Change-Id: I2f8c04e324e45f532c6f413c8c9559654fb094a3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -188,7 +188,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-09-05
+VENDOR_SECURITY_PATCH := 2019-10-01
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
