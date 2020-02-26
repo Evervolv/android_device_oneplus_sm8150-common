bcfde42ff5469dbd82549038f51c2a729f3de7bd
Ashwin R C <ashwin2001achu@gmail.com>
2020-02-26 16:24:12 +0000

    sm8150-common: update to OnePlus7Pro OpenBeta 11
    Change-Id: I42b095a0fcc8df1e3fcc4da335952dc22abda443

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -191,7 +191,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := "2020-01-01"
+VENDOR_SECURITY_PATCH := 2020-03-01
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
