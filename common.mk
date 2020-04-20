b57ba509cacf85e03fdeb399c62afc82f0d91bef
Weikai Kong <pig.priv@gmail.com>
2020-04-20 04:28:44 +0200

    sm8150-common: Properly fix soong namespace issues
    Change-Id: I046ab170e8d8ee27e21c58dad7634a5f13bb39b7

--- common.mk
+++ common.mk
@@ -265,9 +265,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.oneplus_msmnile \
     vendor.evervolv.biometrics.fingerprint.inscreen@1.0-service.oneplus_msmnile \
-    vendor.evervolv.biometrics.fingerprint.inscreen.xml \
-    vendor.oneplus.fingerprint.extension@1.0.vendor \
-    vendor.oneplus.hardware.display@1.0.vendor
+    vendor.evervolv.biometrics.fingerprint.inscreen.xml
 
 # For config.fs
 PRODUCT_PACKAGES += \
