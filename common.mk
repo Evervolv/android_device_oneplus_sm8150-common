6b0e4835946f83512a8308a07c46ffa41cec5605
Nick Reuter <nreuter85@gmail.com>
2020-04-30 22:45:11 -0500

    sm8150-common: update perf blobs
    Change-Id: I1d3da248e06acfb966968c163ee17098835b8962

--- common.mk
+++ common.mk
@@ -412,6 +412,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Qualcomm
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect
