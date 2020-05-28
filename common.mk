bb1ded4918c92c5b783341801be95a436ed709a3
Nick Reuter <nreuter85@gmail.com>
2020-05-27 21:55:15 -0500

    sm8150-common: rollback to prebuilt gps blobs
    Change-Id: Icdc8cc6c338558a8b6e01ca6d85932f4fefb87f6

--- common.mk
+++ common.mk
@@ -277,21 +277,14 @@ PRODUCT_PACKAGES += \
     fs_config_files
 
 # GPS
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.0-impl-qti \
-    android.hardware.gnss@2.0-service-qti \
-    libbatching \
-    libgeofencing \
-    libgnss \
-    libgps.utils \
-    libloc_core \
-    liblocation_api
-
-# GPS config files
-PRODUCT_PACKAGES += \
-    flp.conf \
-    gps.conf \
-    izat.conf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
+    $(LOCAL_PATH)/configs/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
+    $(LOCAL_PATH)/configs/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
+    $(LOCAL_PATH)/configs/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(LOCAL_PATH)/configs/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(LOCAL_PATH)/configs/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    $(LOCAL_PATH)/configs/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
 # Health
 PRODUCT_PACKAGES += \
