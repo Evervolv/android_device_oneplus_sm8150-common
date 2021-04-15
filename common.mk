77e2bcba77be390196ef946e52001ef7708acbf2
Nick Reuter <nreuter85@gmail.com>
2021-04-14 21:03:30 -0500

    sm8150-common: build vendor.qti.hardware.audiohalext@1.0 from source
    Change-Id: Ic5098ae947168791d341427c606f2fc32c27cf30

--- common.mk
+++ common.mk
@@ -105,6 +105,7 @@ PRODUCT_PACKAGES += \
     libtinycompress \
     libvolumelistener \
     tinymix \
+    vendor.qti.hardware.audiohalext@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
