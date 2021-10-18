86fc46c952445e502ae52dc34cc35a7a0c15fd96
TheScarastic <warabhishek@gmail.com>
2021-10-18 08:05:24 +0200

    sm8150-common: build 2.3 fingerprint HIDL
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>
    Change-Id: If76152d1fc86f57e9ffb5fb8e4a1ab2d5db98972

--- common.mk
+++ common.mk
@@ -247,7 +247,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.oneplus \
+    android.hardware.biometrics.fingerprint@2.3-service.oneplus \
     vendor.evervolv.biometrics.fingerprint.inscreen@1.0-service.oneplus \
     vendor.evervolv.biometrics.fingerprint.inscreen.xml
 
