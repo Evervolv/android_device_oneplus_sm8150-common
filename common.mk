e0394986740e5311c1181675e92e7ae0a9cc6493
Erfan Abdi <erfangplus@gmail.com>
2021-10-20 17:20:19 +0300

    taimen: libshims: Add shim for camera blobs
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I5e622e68422f5c4da13c0752a7eb3f4b20ddae49

--- common.mk
+++ common.mk
@@ -435,10 +435,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.servicetracker@1.0.vendor
 
-# Shims
-PRODUCT_PACKAGES += \
-    lib-imsvtshim
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
