c075e4afa33c3cd71dfce507743d72695d701afd
Nick Reuter <nreuter85@gmail.com>
2021-12-02 20:31:32 -0600

    Revert "sm8150-common: Override vbmeta version in kernel cmdline"
    This reverts commit 81f811438a61884f6b5e158e7a5b2a49168a0b0a.
    
    Change-Id: Ic3634958b7ea361b15eafb4b6772c3d1393853ee

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,7 +87,6 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
-    androidboot.vbmeta.avb_version=1.0 \
     loop.hw_queue_depth=31 \
     loop.max_part=7 \
     lpm_levels.sleep_disabled=1 \
