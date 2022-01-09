12942e075feb9ed251f0d04a074730864cd36158
Pranav <npv12@iitbbs.ac.in>
2022-01-09 15:56:26 +0530

    sm8150-common: Sensors: Conditionally enable debug logs
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>
    Change-Id: I774c1309cc9490a3f11375ea2806d403c97cad5f

--- sensors/AlsCorrection.cpp
+++ sensors/AlsCorrection.cpp
@@ -27,6 +27,8 @@ namespace sensors {
 namespace V2_1 {
 namespace implementation {
 
+static bool DEBUG = false;
+
 static int red_max_lux, green_max_lux, blue_max_lux, white_max_lux, max_brightness;
 static int als_bias;
 
@@ -46,20 +48,29 @@ void AlsCorrection::init() {
     white_max_lux = get("/mnt/vendor/persist/engineermode/white_max_lux", 0);
     als_bias = get("/mnt/vendor/persist/engineermode/als_bias", 0);
     max_brightness = get("/sys/class/backlight/panel0-backlight/max_brightness", 255);
-    ALOGV("max r = %d, max g = %d, max b = %d", red_max_lux, green_max_lux, blue_max_lux);
+    if(DEBUG) {
+        ALOGV("max r = %d, max g = %d, max b = %d", red_max_lux, green_max_lux, blue_max_lux);
+    }
 }
 
 void AlsCorrection::correct(float& light) {
+    if(DEBUG) {
+        ALOGV("Original reading: %f", light);
+    }
+
     pid_t pid = property_get_int32("vendor.sensors.als_correction.pid", 0);
     if (pid != 0) {
         kill(pid, SIGUSR1);
     }
+
     // TODO: HIDL service and pass float instead
     int r = property_get_int32("vendor.sensors.als_correction.r", 0);
     int g = property_get_int32("vendor.sensors.als_correction.g", 0);
     int b = property_get_int32("vendor.sensors.als_correction.b", 0);
-    ALOGV("Screen Color Above Sensor: %d, %d, %d", r, g, b);
-    ALOGV("Original reading: %f", light);
+    if(DEBUG) {
+        ALOGV("Screen Color Above Sensor: %d, %d, %d", r, g, b);
+    }
+
     int screen_brightness = get("/sys/class/backlight/panel0-backlight/brightness", 0);
     float correction = 0.0f, correction_scaled = 0.0f;
     if (red_max_lux > 0 && green_max_lux > 0 && blue_max_lux > 0 && white_max_lux > 0) {
@@ -77,6 +88,7 @@ void AlsCorrection::correct(float& light) {
         correction_scaled = correction * (((float) white_max_lux) /
                                           (red_max_lux + green_max_lux + blue_max_lux));
     }
+
     if (light - correction >= 0) {
         // Apply correction if light - correction >= 0
         light -= correction;
@@ -91,7 +103,10 @@ void AlsCorrection::correct(float& light) {
         // computed correction instead allows to fix the issue
         light = correction;
     }
-    ALOGV("Corrected reading: %f", light);
+
+    if(DEBUG) {
+        ALOGV("Corrected reading: %f", light);
+    }
 }
 
 }  // namespace implementation
