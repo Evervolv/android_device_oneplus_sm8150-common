eb684d6f1fbabe603aedd216d4f45eaf00f5f55a
Alexander Koskovich <akoskovich@pm.me>
2022-05-12 01:06:04 -0700

    sm8150-common: init: Set model based on RF version
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Change-Id: I06f9acda3fe77f57a1bc24ee5a802db0ab3b8d60

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -75,6 +75,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_oplus
+
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
