b06340151c99bf1c8d017a2dfae945edebfdcb5c
Sandeep Dhavale <dhavale@google.com>
2022-10-20 06:56:49 +0000

    sm8150-common: Use Fastboot AIDL implementation of pixel
    Bug: 205760652
    Test: build
    
    Change-Id: Iccce864539905dd7edf3672e4725505cbe3bdf79
    Signed-off-by: Sandeep Dhavale <dhavale@google.com>

--- common.mk
+++ common.mk
@@ -160,7 +160,7 @@ PRODUCT_PACKAGES += \
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl.custom \
+    android.hardware.fastboot-service.custom_recovery \
     fastbootd
 
 # Fingerprint
