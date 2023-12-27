b3355bcd42c5978cc04bf8c2fd0693f2ef4b4919
Suren Baghdasaryan <surenb@google.com>
2023-12-27 08:51:53 +0300

    sm8150-common: Replace writepid with task_profiles command  for cgroup migration
    writepid command usage to join a cgroup has been deprecated in favor
    of a more flexible approach using task_profiles. This way cgroup path
    is not hardcoded and cgroup changes can be easily made. Replace
    writepid with task_profiles command to migrate between cgroups.
    
    Bug: 191283136
    Test: build and boot
    Change-Id: I12fe7186af1ee5db6f19d485987e78ae0792c606
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- extract-files.py
+++ extract-files.py
@@ -55,6 +55,8 @@ blob_fixups: blob_fixups_user_type = {
         .patch_file('blob-patches/manifest_oplus_fingerprint.patch'),
     ('odm/lib64/mediadrm/libwvdrmengine.so', 'odm/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    ('odm/etc/init/android.hardware.drm@1.3-service.widevine.rc', 'vendor/etc/init/vendor.qti.media.c2@1.0-service.rc'): blob_fixup()
+        .regex_replace('writepid /dev/cpuset/foreground/tasks', 'task_profiles ProcessCapacityHigh'),
     'product/app/PowerOffAlarm/PowerOffAlarm.apk': blob_fixup()
         .apktool_patch('blob-patches/PowerOffAlarm.patch', '-s'),
     'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()
@@ -74,6 +76,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('NXP_NFC_DEV_NODE="/dev/pn553"', 'NXP_NFC_DEV_NODE="/dev/nq-nci"')
         .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02')
         .regex_replace('NFC_DEBUG_ENABLED=0x01', 'NFC_DEBUG_ENABLED=0x00'),
+    'vendor/etc/init/android.hardware.neuralnetworks@1.3-service-qti.rc': blob_fixup()
+        .regex_replace('writepid /dev/stune/nnapi-hal/tasks', 'task_profiles NNApiHALPerformance'),
     'vendor/lib64/hw/com.qti.chi.override.so': blob_fixup()
         .add_needed('libcamera_metadata_shim.so'),
 }  # fmt: skip
