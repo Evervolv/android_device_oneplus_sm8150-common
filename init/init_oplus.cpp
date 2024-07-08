d6d148eee8a371438d9bff66bd515db11a618f01
Nick Reuter <nreuter85@gmail.com>
2024-07-08 11:44:08 -0500

    sm8150-common: init: add support for carrier branded devices
    Change-Id: Ic08dd6dd9c43fd8c422b7361d8d1aff442b24f12

--- init/init_oplus.cpp
+++ init/init_oplus.cpp
@@ -24,6 +24,34 @@ constexpr const char* RO_PROP_SOURCES[] = {
     "vendor_dlkm.",
 };
 
+constexpr const char* BUILD_DEVICE[] = {
+    "OP7ProNRSpr",
+    "OnePlus7ProNR",
+    "OnePlus7ProTMO",
+    "OnePlus7TProNR",
+};
+
+constexpr const char* BUILD_PRODUCT[] = {
+    "OP7ProNRSpr",
+    "OnePlus7ProNR_EEA",
+    "OnePlus7ProTMO",
+    "OnePlus7TProNR",
+};
+
+constexpr const char* BUILD_DESCRIPTION[] = {
+    "OP7ProNRSpr-user 10 QKQ1.190716.003 2204192115 release-keys",
+    "OnePlus7ProNR-user 10 QKQ1.190716.003 2109160130 release-keys",
+    "OnePlus7ProTMO-user 11 RKQ1.201022.002 2204011431 release-keys",
+    "OnePlus7TProNR-user 11 RKQ1.201022.002 2108021431 release-keys",
+};
+
+constexpr const char* BUILD_FINGERPRINT[] = {
+    "OnePlus/OP7ProNRSpr/OP7ProNRSpr:10/QKQ1.190716.003/2204192115:user/release-keys",
+    "OnePlus/OnePlus7ProNR_EEA/OnePlus7ProNR:10/QKQ1.190716.003/2109160130:user/release-keys",
+    "OnePlus/OnePlus7ProTMO/OnePlus7ProTMO:11/RKQ1.201022.002/2204011431:user/release-keys",
+    "OnePlus/OnePlus7TProNR/OnePlus7TProNR:11/RKQ1.201022.002/2108021431:user/release-keys",
+};
+
 /*
  * SetProperty does not allow updating read only properties and as a result
  * does not work for our use case. Write "OverrideProperty" to do practically
@@ -40,7 +68,7 @@ void OverrideProperty(const char* name, const char* value) {
     }
 }
 
-void SetDeviceModel(const char* model) {
+void SetDeviceModel(int project, int model_num, int carrier) {
     const auto ro_prop_override = [](const char* source, const char* prop, const char* value,
                                      bool product) {
         std::string prop_name = "ro.";
@@ -53,51 +81,88 @@ void SetDeviceModel(const char* model) {
         OverrideProperty(prop_name.c_str(), value);
     };
 
+    std::string model = project >= 18865 ? "HD" : "GM";
+    model += std::to_string(model_num);
+
     for (const auto& source : RO_PROP_SOURCES) {
-        ro_prop_override(source, "model", model, true);
+        ro_prop_override(source, "model", model.c_str(), true);
     }
+
+    if (carrier < 0 || carrier > 3) {
+        return;
+    }
+
+    for (const auto& source : RO_PROP_SOURCES) {
+        ro_prop_override(source, "name", BUILD_PRODUCT[carrier], true);
+        ro_prop_override(source, "device", BUILD_DEVICE[carrier], true);
+        ro_prop_override(source, "fingerprint", BUILD_FINGERPRINT[carrier], false);
+    }
+    ro_prop_override(nullptr, "product", BUILD_PRODUCT[carrier], false);
+    ro_prop_override(nullptr, "description", BUILD_DESCRIPTION[carrier], false);
 }
 
 void vendor_load_properties() {
+    if (access("/system/bin/recovery", F_OK) == 0) {
+        return;
+    }
+
     auto project = std::stoi(GetProperty("ro.boot.prjname", ""));
+    auto rf_version = std::stoi(GetProperty("ro.boot.rf_version", ""));
+    int carrier = -1;
     int model_num = -1;
     switch (project) {
         case 18825: // 7 Pro 5g Sprint
             model_num = 1925;
+            carrier = 0;
             break;
         case 18827: // 7 Pro 5g
             model_num = 1920;
+            carrier = 1;
+            break;
+        case 18821: // 7 Pro
+            if (rf_version == 2) {
+                model_num = 1915;
+                carrier = 2;
+            }
+            break;
+        case 18831: // 7 Pro T-Mobile
+            model_num = 1915;
+            carrier = 2;
+            break;
+		case 19861: // 7T Pro 5g
+            model_num = 1925;
+            carrier = 3;
             break;
         default:
-            auto rf_version = std::stoi(GetProperty("ro.boot.rf_version", ""));
-            switch (rf_version) {
-                case 1: // China
-                    model_num = 1900;
-                    break;
-                case 2: // T-Mobile
-                    model_num = 1915;
-                    break;
-                case 3: // India
-                    model_num = 1901;
-                    break;
-                case 4: // Europe
-                    model_num = 1903;
-                    break;
-                default: // Global / US Unlocked
-                    model_num = 1907;
-                    break;
-            }
-            if (project >= 18865) {
-                model_num += 10;
-            }
             break;
     }
 
-    if (model_num == -1) {
+    if (model_num != -1 && carrier != -1) {
+        SetDeviceModel(project, model_num, carrier);
         return;
     }
 
-    std::string model = project >= 18865 ? "HD" : "GM";
-    model += std::to_string(model_num);
-    SetDeviceModel(model.c_str());
+    switch (rf_version) {
+        case 1: // China
+            model_num = 1900;
+            break;
+        case 2: // T-Mobile
+            model_num = 1915;
+            break;
+        case 3: // India
+            model_num = 1901;
+            break;
+        case 4: // Europe
+            model_num = 1903;
+            break;
+        default: // Global / US Unlocked
+            model_num = 1907;
+            break;
+    }
+
+    if (project >= 18865) {
+        model_num += 10;
+    }
+
+    SetDeviceModel(project, model_num, carrier);
 }
