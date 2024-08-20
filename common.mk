e9e4086ef29866c10f414e9cbb43b903e57fc9ab
Albert Tang <tangalbert919@yahoo.com>
2024-08-20 08:14:22 +0530

    sm8150-common: Import task_profiles.json
    Change-Id: I922e83abeb47706303f2b83698ea07c09a65b069

--- common.mk
+++ common.mk
@@ -257,7 +257,8 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
