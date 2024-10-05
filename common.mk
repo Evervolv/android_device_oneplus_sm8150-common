28a095f85db7ba69c6b14a8269804334c66cd67a
pjgowtham <pjgowtham@gmail.com>
2024-10-05 19:08:53 +0530

    sm8150-common: Avoid building oplus doze
    AOSP doze already covers most of the features.
    
    What is not supported by AOSP is
    - Using pickup sensor for doze pulse
    - Toggling quick pickup sensor irrespective of pickup gesture
    
    But these are probably not the features that we'd want to expose.
    
    Change-Id: I8655baceb4be3e3a68613486da4ac4c0c5ff93d6

--- common.mk
+++ common.mk
@@ -118,11 +118,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-
-# Doze
-PRODUCT_PACKAGES += \
-    OplusDoze
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 # DRM
 PRODUCT_PACKAGES += \
