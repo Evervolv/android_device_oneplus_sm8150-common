d49ce2721247154b673d8a7f2df8172b4256da71
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:21:39 +0100

    sm8150-common: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- common.mk
+++ common.mk
@@ -166,6 +166,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_oplus)
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
